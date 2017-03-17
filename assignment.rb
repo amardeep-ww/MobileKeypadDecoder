446b7e18a5a481a47aed8d8ac58b60ea19d6e7b1
amardeep-ww <amardeep.jadhav@wwindia.com>
2017-03-17 16:50:22 +0530

    created decode method to which find all permutations and match with dictionary array and search for valid words

--- assignment.rb
+++ assignment.rb
@@ -16,6 +16,7 @@ class Moblile_keypad_decoder
   def initialize(phone_number)
     combinations = split_number(phone_number)
     decoded_words = load_dictionary
+    decode(combinations, decoded_words)
   end
 
   def split_number(phone_number)
@@ -34,8 +35,42 @@ class Moblile_keypad_decoder
   end
 
   def load_dictionary
-    d = File.read('dictionary.txt').downcase.split("\n")
-    binding.pry
+    File.read('dictionary.txt').downcase.split("\n")
+  end
+
+  def find_all_permutations(combination, decoded_words)
+    arr = []
+    combination.each do |x|
+      unless x.empty?
+        words = get_words(x, decoded_words)
+        arr.push(words)
+      end
+    end
+    arr
+  end
+
+  def get_words(number, decoded_words)
+    words = []
+    x = []
+    number.split('').each do |var|
+      x.push(@@letters_hash[var])
+    end
+
+    unless x.empty?
+      x[0].product(*x[1..-1]).each do |val|
+        word = val.join
+        words.push(word) if decoded_words.include? word
+      end
+    end
+    words
+  end
+
+  def decode(combinations, decoded_words)
+    result = []
+    combinations.each do |combination|
+      arr = []
+      words = find_all_permutations(combination, decoded_words)
+    end
   end
 
 end
