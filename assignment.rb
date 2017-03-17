38d54a9e199bd81967580a39461b3879563a33fd
amardeep-ww <amardeep.jadhav@wwindia.com>
2017-03-17 14:43:22 +0530

    created split number function for split the phone number in to number of possible combination of 2 partition of phome number

--- assignment.rb
+++ assignment.rb
@@ -14,9 +14,23 @@ class Moblile_keypad_decoder
   }
 
   def initialize(phone_number)
-    # initialization and business logic
+    combinations = split_number(phone_number)
   end
+
+  def split_number(phone_number)
+    combinations = []
+    combinations << array_split(phone_number, 3)
+    combinations << array_split(phone_number, 4)
+    combinations << array_split(phone_number, 5)
+    combinations << array_split(phone_number, 6)
+    combinations << array_split(phone_number, 7)
+    combinations << array_split(phone_number, 10)
+    combinations
+  end
+
+  def array_split(phone_number, length)
+    [phone_number.slice(0, length), phone_number.slice(length, 10 - length)]
+  end
+
 end
 Moblile_keypad_decoder.new('2282668625')
-
-
